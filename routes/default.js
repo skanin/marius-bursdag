b7d23116b71f2c106a686b7f39af48c136585ad3
Sander Bjerklund Lindberg <sander.b.lindberg@gmail.com>
2021-10-12 16:02:32 +0200

    Add enpoint

--- routes/default.js
+++ routes/default.js
@@ -10,18 +10,23 @@ router.get('/', (req, res) => {
 });
 
 router.get('/img',(req, res) => {
-    const r = fs.createReadStream('./hall_of_masters.png') // or any other way to get a readable stream
-    const ps = new stream.PassThrough() // <---- this makes a trick with stream error handling
-    stream.pipeline(
-     r,
-     ps, // <---- this makes a trick with stream error handling
-     (err) => {
-      if (err) {
-        console.log(err) // No such file or any other kind of error
-        return res.sendStatus(400); 
-      }
-    })
-    ps.pipe(res) // <---- this makes a trick with stream error handling
-  })
+    // const r = fs.createReadStream('./hall_of_masters.png') // or any other way to get a readable stream
+    // const ps = new stream.PassThrough() // <---- this makes a trick with stream error handling
+    // stream.pipeline(
+    //  r,
+    //  ps, // <---- this makes a trick with stream error handling
+    //  (err) => {
+    //   if (err) {
+    //     console.log(err) // No such file or any other kind of error
+    //     return res.sendStatus(400); 
+    //   }
+    // })
+    // ps.pipe(res) // <---- this makes a trick with stream error handling
+    res.sendFile('hall_of_masters.png', {root: __dirname})
+})
+
+router.get('/jegangrerpaaatjegjuksetpaasnake', (req, res) => {
+    res.sendFile('views/det_bor_du.html', {root: __dirname})
+})
 
 module.exports = router;
